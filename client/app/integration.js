ed9c0b114e2b828a6b89aaae60282844da11a544
József Kozma <jozsef.kozma@gmail.com>
2015-10-20 10:13:09 +0200

    fix(config): Emarsys.config was not available

--- client/app/integration.js
+++ client/app/integration.js
@@ -1,11 +1,14 @@
 'use strict';
 
+var extend = require('extend');
+
 var EmarsysApi = require('./service/emarsys_api');
 var DialogApi = require('./service/dialog_api');
 var EventListener = require('./message_handler/listener');
 
 (function(global) {
-  global.Emarsys = global.Emarsys || {};
+  global.Emarsys = global.Emarsys || (global.SUITE ? extend(true, {}, global.SUITE) : {});
+  global.Emarsys.config = global.Emarsys.config || (global.SUITE.config ? extend(true, {}, global.SUITE.config) : {});
   var emarsysApi = new EmarsysApi({
     global: global,
     integrationId: 'EMARSYS',
