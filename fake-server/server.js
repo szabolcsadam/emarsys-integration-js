42d07f5d08f6e5bcc64bcc7a47217187a26c6927
József Kozma <jozsef.kozma@gmail.com>
2015-10-30 16:04:15 +0100

    feat(fake-server): telling which port it is using

--- fake-server/server.js
+++ fake-server/server.js
@@ -2,8 +2,14 @@ var Static = require('node-static');
 
 var fileServer = new Static.Server(__dirname + '/../dist', { cache: 0 });
 
+var port = 1235;
+
 require('http').createServer(function(request, response) {
     request.addListener('end', function() {
         fileServer.serve(request, response);
     }).resume();
-}).listen(1235);
+}).listen(port);
+
+console.log('Application started:', {
+    port: port
+});
