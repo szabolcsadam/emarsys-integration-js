44466763db23a99190f9d452c120678c553531ef
rudywaltz <david.barkoczi@gmail.com>
2017-07-04 14:19:28 +0200

    test(dialogFactory): confirm and modal instance

--- client/app/service/dialog_factory.spec.js
+++ client/app/service/dialog_factory.spec.js
@@ -1,15 +1,26 @@
 'use strict';
 
 const DialogFactory = require('./dialog_factory');
+const fakeMessage = { data: { title: 'foo' } };
 
 describe('DialogFactory', function() {
+  let dialogFactory;
 
-  describe('#create', function() {
-    it('should create a factory instance', function() {
-      const dialogFactory = DialogFactory.create();
-      expect(dialogFactory).to.respondTo('createConfirmDialog');
-      expect(dialogFactory).to.respondTo('createModalDialog');
+  beforeEach(function() {
+    dialogFactory = DialogFactory.create();
+  });
+
+  describe('#createConfirmDialog', function() {
+    it('should create a Confirm instance', function() {
+      const dialog = dialogFactory.createConfirmDialog('global', fakeMessage);
+      expect(dialog.constructor.name).to.equal('Confirm');
     });
   });
 
+  describe('#createModalDialog', function() {
+    it('should create a Modal instance', function() {
+      const modal = dialogFactory.createModalDialog('global', fakeMessage);
+      expect(modal.constructor.name).to.equal('Modal');
+    });
+  });
 });
